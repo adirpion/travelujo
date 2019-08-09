6387e62965af820244d8c321c1a85a4f7d5c549f
Adir <adirpeer@gmail.com>
2019-08-09 10:28:40 +0300

    build the dekstop header

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -4,13 +4,14 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),    
 cssnested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');    
+mixins = require('postcss-mixins'),
+hexTorgba = require('postcss-hexrgba');    
 
 
 gulp.task('styles', function(){
     
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssnested , cssvars , autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssnested , cssvars , hexTorgba , autoprefixer]))
         .on('error' , function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
